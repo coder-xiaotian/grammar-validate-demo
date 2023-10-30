88a4e1365998469c21243feab8169d4b5e2f8432
xiaotian <wanxiaotian888@gmail.com>
2023-10-30 17:00:11 +0800

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,109 @@
-import Image from 'next/image'
+"use client"
+import peg from 'pegjs'
+import { useRef } from 'react'
+import Editor, {useMonaco} from '@monaco-editor/react';
 
+const parser = peg.generate(`
+program = functionCall
+
+functionCall = fn:$chars beginBracket head:paramType? tail:(comma restP:paramType {return restP})* endBracket {
+  let params = ""
+  const paramList = []
+  if (head) {
+    params += head
+    paramList.push(head)
+  }
+  if (tail) {
+    tail.forEach(s => {
+      params += "," + s
+      paramList.push(s)
+    })
+  }
+  const error = options.validateFunctionName(fn, paramList)
+  if (error) {
+    return expected(error)
+  }
+  return {
+    type: "CallExpression",
+    callee: {
+      type: "Identifier",
+      value: fn
+    },
+    arguments: paramList
+  }
+}
+paramType = binaryExpr/string/number/columnName/functionCall
+binaryExpr = l:(string/number/columnName/functionCall) o:operType r:(string/number/columnName/functionCall) {
+  return {
+    type: "BinaryExpression",
+    operator: o,
+    left: l,
+    right: r
+  }
+}
+operType = "=="/">"/"<"/"!="/"<="/">="
+string = doubleQuotationMark s:chars doubleQuotationMark {
+  return {
+    type: "StringLiteral",
+    value: s
+  }
+}
+chars = c:[a-z]i+ {
+  return c.join("")
+}
+number = n:[0-9]+ {
+  return {
+    type: "NumberLiteral",
+    value: Number(n.join("")),
+  }
+}
+columnName = "\`" c:chars "\`" {
+  return {
+  	type: "ColumnLiteral",
+    value: c
+  }
+}
+
+doubleQuotationMark = '"'
+beginBracket = '('
+endBracket = ')'
+beginBrace = '{'
+endBrace = '}'
+ws = " "
+comma = ","
+  `)
+  
 export default function Home() {
+  const monaco = useMonaco()
+  const editorRef = useRef()
+  function handleChange(value) {
+    try {
+      const res = parser.parse(value, {
+        validateFunctionName(funName, params) {
+          console.log(funName, params)
+          if (funName !== "ADD") {
+            return "没有该函数"
+          }
+        }
+      })
+      console.log(res)
+      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", [])
+    } catch(e) {
+      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", [{
+        message: e.message,
+				severity: monaco.MarkerSeverity.Error,
+				startLineNumber: e.location.start.line,
+				startColumn: e.location.start.column,
+				endLineNumber: e.location.end.line,
+				endColumn: e.location.end.column,
+      }])
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Editor height="90vh" defaultLanguage="javascript"
+      onChange={handleChange}
+      onMount={(editor) => editorRef.current = editor}
+    />
   )
 }
