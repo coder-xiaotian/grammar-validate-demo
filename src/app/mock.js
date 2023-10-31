4a559fdea585f84866be89098f7b96f9ef04bc2a
wuchunyi <3248947372@qq.com>
2023-10-31 18:34:37 +0800

    增加参数校验

---
+++ src/app/mock.js
@@ -0,0 +1,20 @@
+export const functions = {
+  SUM: {
+    desc: "总和",
+    params: [
+      {
+        name: "`列名`",
+        valueType: { column: ["number"], constant: ["string"] },
+        desc: "数值列",
+      },
+    ],
+    result: { number: "single" },
+  },
+};
+
+export const table = [
+  { name: "column1", valueType: "number" },
+  { name: "column2", valueType: "string" },
+  { name: "column3", valueType: "number" },
+  { name: "column4", valueType: "time" },
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
-"use client"
-import peg from 'pegjs'
-import { useRef } from 'react'
-import Editor, {useMonaco} from '@monaco-editor/react';
+"use client";
+import peg from "pegjs";
+import { useRef } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { table, functions } from "./mock";
+
+const DATA_TYPE = {
+  StringLiteral: "string",
+  NumberLiteral: "number",
+  ColumnLiteral: "column",
+};
 
 const parser = peg.generate(`
 {
@@ -47,6 +54,7 @@ functionCallExpr = fn:$chars beginBracket head:paramType? tail:(comma restP:para
     return expected(error)
   }
   return {
+
     type: "CallExpression",
     callee: {
       type: "Identifier",
@@ -93,7 +101,7 @@ string = doubleQuotationMark s:chars doubleQuotationMark {
     value: s
   }
 }
-chars = c:[a-z\u4e00-\u9fa5]i+ {
+chars = c:[0-9a-z\u4e00-\u9fa5]i+ {
   return c.join("")
 }
 number = n:[0-9]+ {
@@ -103,11 +111,18 @@ number = n:[0-9]+ {
   }
 }
 columnName = "\`" c:chars "\`" {
+
+  const error = options.validateColumnName(c)
+  if (error) {
+    return expected(error)
+  }
+  
   return {
   	type: "ColumnLiteral",
     value: c
   }
 }
+
 
 doubleQuotationMark = '"'
 beginBracket = '('
@@ -116,42 +131,106 @@ beginBrace = '{'
 endBrace = '}'
 ws = " "
 comma = ","
-  `)
-  
+  `);
+
+export const isNotTableColumnType = (columnName, columnTypes, table) => {
+  return !table.some(
+    (el) => el.name === columnName && columnTypes.includes(el.valueType)
+  );
+};
+
+export const isNotTableColumnName = (columnName, table) => {
+  return !table.some((el) => el.name === columnName);
+};
+
+export const isNotDefineDataType = (stringType, dataTypes) => {
+  return !dataTypes.includes(stringType);
+};
+
+export const getParamLen = (params) => {
+  return params.filter((el) => !el.optional).length;
+};
+
 export default function Home() {
-  const monaco = useMonaco()
-  const editorRef = useRef()
+  const monaco = useMonaco();
+  const editorRef = useRef();
   function handleChange(value) {
     try {
       const res = parser.parse(value, {
         validateFunctionName(funName, params) {
-          console.log(funName, params)
-          if (funName !== "ADD") {
-            return "没有该函数"
+          // 参数是否可选走长度校验
+
+          console.log(funName, params, "params");
+          if (!functions[funName]) {
+            return `不存在函数${funName}`;
           }
-        }
-      })
-      console.log(res)
-      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", [])
-    } catch(e) {
-      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", [{
-        message: e.message,
-				severity: monaco.MarkerSeverity.Error,
-				startLineNumber: e.location.start.line,
-				startColumn: e.location.start.column,
-				endLineNumber: e.location.end.line,
-				endColumn: e.location.end.column,
-      }])
+          const paramLen = getParamLen(functions[funName].params);
+
+          for (let i = 0; i < params.length; i++) {
+            if (i + 1 > paramLen) {
+              return `${funName}函数参数不超过${paramLen}个`;
+            }
+
+            const param = params[i];
+            const columnTypes = functions[funName].params[i].valueType.column,
+              constantTypes = functions[funName].params[i].valueType.constant;
+            let isNotTrueType = false;
+            if (columnTypes && constantTypes) {
+              isNotTrueType =
+                isNotTableColumnType(param.value, columnTypes, table) &&
+                isNotDefineDataType(DATA_TYPE[param.type], constantTypes);
+              if (isNotTrueType) {
+                return `参数类型应该是column或者是${constantTypes.join(",")}`;
+              }
+            } else if (columnTypes) {
+              isNotTrueType = isNotTableColumnType(
+                param.value,
+                columnTypes,
+                table
+              );
+              if (isNotTrueType) {
+                return `参数类型应该是column`;
+              }
+            } else {
+              isNotTrueType = isNotDefineDataType(
+                DATA_TYPE[param.type],
+                constantTypes
+              );
+              if (isNotTrueType) {
+                return `参数类型应该是${constantTypes.join(",")}`;
+              }
+            }
+          }
+        },
+        validateColumnName(columnName) {
+          if (isNotTableColumnName(columnName, table)) {
+            return `没有该列名：${columnName}`;
+          }
+        },
+      });
+      console.log(res);
+      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", []);
+    } catch (e) {
+      monaco.editor.setModelMarkers(editorRef.current.getModel(), "owner", [
+        {
+          message: e.message,
+          severity: monaco.MarkerSeverity.Error,
+          startLineNumber: e.location.start.line,
+          startColumn: e.location.start.column,
+          endLineNumber: e.location.end.line,
+          endColumn: e.location.end.column,
+        },
+      ]);
     }
   }
 
   return (
-    <Editor 
-      height="90vh" 
+    <Editor
+      height="90vh"
       defaultLanguage="javascript"
-      defaultValue='`金额`+(SUM(IF(`活动名称`=="办案计划书",`金额`))-AVG(IF(`活动名称`=="立案信息",`金额`)))*COUNT(IF(`活动名称`=="立案信息",`金额`))'
+      // defaultValue='`金额`+(SUM(IF(`活动名称`=="办案计划书",`金额`))-AVG(IF(`活动名称`=="立案信息",`金额`)))*COUNT(IF(`活动名称`=="立案信息",`金额`))'
       onChange={handleChange}
-      onMount={(editor) => editorRef.current = editor}
+      onMount={(editor) => (editorRef.current = editor)}
     />
-  )
+  );
 }
